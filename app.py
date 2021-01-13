2693bb7fb08857159ee878cf2fa7cd42b1db69ea
icekunx <icekunxp@gmail.com>
2021-01-13 12:12:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
     except Exception:
         return "Could not read to file"
     
-    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/"
+    return "Ice kunxx"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
